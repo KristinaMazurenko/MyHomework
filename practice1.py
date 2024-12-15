75e56017a07fea5a84a0fc64846ee09808b74f9f
Kristina Mazurenko <km.kris.km5@gmail.com>
2024-12-15 14:52:49 +0300

    h1 - Операции над строками, Индексы и срезы строк

---
+++ practice1.py
@@ -0,0 +1,109 @@
+# Темы: Операции над строками, Индексы и срезы строк
+# Результат по каждому заданию необходимо выводить в консоль через print().
+
+# 1. Объедините строки "Hello" и "London" без пробела. Ожидаемый результат: "HelloLondon"
+
+str_uno = 'Hello'
+str_duo = 'London'
+print(f'{str_uno}{str_duo}')
+
+# 2. Выведите последний символ строки "Programming". Ожидаемый результат: "g"
+
+str_tre = 'Programming'
+print(str_tre[10])
+
+# 3. Дублируйте строку "Hi" три раза. Ожидаемый результат: "HiHiHi"
+
+str_quattro = 'Hi'
+print(str_quattro * 3)
+
+# 4. Определите длину строки "Artificial Intelligence".
+
+str_cinque = 'Artificial Intelligence'
+print(len(str_cinque))
+
+# 5. Объедините строки "Good" и "Morning" с запятой между ними. Ожидаемый результат: "Good,Morning"
+
+str_sei = 'Good'
+str_sette = 'Morning'
+print(f'{str_sei},{str_sette}')
+
+# 6. Создайте срез строки "Natural Language Processing" со значением "nguag".
+
+str_otto = 'Natural Language Processing'
+print(str_otto[10:15])
+
+# 7. Объедините строки "Data" и "Science" с дефисом между ними. Ожидаемый результат: "Data-Science"
+
+str_nove = 'Data'
+str_dieci = 'Science'
+print(f'{str_nove}-{str_dieci}')
+
+# 8. Объедините строки "AI" и "ML" с двоеточием между ними. Ожидаемый результат: "AI:ML"
+
+str_undici = 'AI'
+str_dodici = 'ML'
+print(f'{str_undici}:{str_dodici}')
+
+# 9. Дублируйте строку "Test" шесть раз. Ожидаемый результат: "TestTestTestTestTestTest"
+
+str_tredici = 'Test'
+print(str_tredici * 6)
+
+# 10. Выведите первый символ строки "Python". Ожидаемый результат: "P"
+
+str = 'Python'
+print(str[0])
+
+# 11. Создайте срез строки "Hello, Anna!" от 0 до 5. Ожидаемый результат: "Hello"
+
+str_one = 'Hello, Anna!'
+print(str_one[0:5])
+
+# 12. Определите длину строки "Natural Language Processing".
+
+str_two = 'Natural Language Processing'
+print(len(str_two))
+
+# 13. Выведите второй символ строки "Лето". Ожидаемый результат: "е"
+
+str_three = 'Лето'
+print(str_three[1])
+
+# 14. Выведите предпоследний символ строки "Machine Learning". Ожидаемый результат: "n"
+
+str_four = 'Machine Learning'
+print(str_four[14])
+
+# 15. Дублируйте строку "Yes" четыре раза. Ожидаемый результат: "YesYesYesYes"
+str_five = 'Yes'
+print(str_five * 4)
+
+# 16. Создайте срез строки "Deep Learning" со значением "ep Le".
+
+str_six = 'Deep Learning'
+print(str_six[2:7])
+
+# 17. Выведите третий символ строки "Computer Vision". Ожидаемый результат: "m"
+
+str_seven = 'Computer Vision'
+print(str_seven[2])
+
+# 18. Определите длину строки "Deep Learning". Ожидаемый результат: 13
+
+str_eight = 'Deep Learning'
+print(len(str_eight))
+
+# 19. Объедините строки "Python" и "Rocks" с пробелом между ними. Ожидаемый результат: "Python Rocks"
+
+str_nine = 'Python'
+str_ten = 'Rocks'
+print(f'{str_nine} {str_ten}')
+
+# 20. Создайте срез строки "Data Science" со значением "cien".
+
+str_eleven = 'Data Science'
+print(str_eleven[6:10])
+
+
+
